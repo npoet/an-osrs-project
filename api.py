2444389c1d8148884d396eb5b886336cf3a6de73
npoet <nicholasjpoet@gmail.com>
2025-09-27 15:17:25 -0700

    add comments

--- api.py
+++ api.py
@@ -3,18 +3,19 @@ import aiohttp
 from typing import Dict, Any
 
 PRICES_BASE_URL = "https://prices.runescape.wiki/api/v1/osrs/latest"
+# Descriptive user-agent per wiki guidelines, don't add me on discord
 USER_AGENT = "aiohttp - @npoet on discord"
 HEADERS = {"User-Agent": USER_AGENT}
 
 ITEM_DICT = {
-    "Avernic treads (max)": [31088, 13239, 13237, 13235],
-    "Imbued zamorak cape (deadman)": [29628],
+    "Avernic treads (max)": [31088, 13239, 13237, 13235],   # Avernic treads + Prims + Pegs + Eternals
+    "Imbued zamorak cape (deadman)": [29628],   # Armageddon cape fabric
     "Confliction gauntlets": [31106],
     "Oathplate chest": [30753],
     "Oathplate legs": [30756],
     "Amulet of rancour": [29801],
-    "Dinh's blazing bulwark": [21015, 28684],
-    "Celestial signet": [23943],
+    "Dinh's blazing bulwark": [21015, 28684],   # Dinh's bulwark + Trailblazer reloaded bulwark orn kit
+    "Celestial signet": [23943],    # Elven signet
 }
 
 
@@ -48,5 +49,6 @@ async def get_total_price(items: Dict[str, Any]) -> int:
 
 
 if __name__ == "__main__":
+    # Calc total price and display as GP
     total = asyncio.run(get_total_price(ITEM_DICT))
     print(f"{total:,} gp")
